4488e2aaa4a52963a99ed79c3f4d677a09531898
Like4Schnitzel <rw21062006@gmail.com>
2023-11-06 21:14:59 +0100

    check for youtube.com url

--- src/lib/Input.svelte
+++ src/lib/Input.svelte
@@ -1,11 +1,27 @@
 <script lang="ts">
     import { queue } from "./stores";
 
-    const processInput = () => {
-        const inputURL = new URL(inputText);
+    function lstrip(s: string, characters: string) {
+        let start = 0;
+        while (characters.indexOf(s[start]) >= 0) {
+            start += 1;
+        }
+        let end = s.length - 1;
+        return s.substring(start, end);
+    }
 
-        if (inputURL)
+
+    const processInput = () => {
+        // make sure there's https:// as a prefix
+        const httpsInputText = "https://" + lstrip(inputText, "https://");
+        const inputURL = new URL(httpsInputText);
+        const inputURLParts = inputURL.hostname.split('.');
+        const partsCount = inputURLParts.length;
+
+        if (inputURL && inputURLParts[partsCount-2] + "." + inputURLParts[partsCount-1] === "youtube.com")
+        {
             queue.add(inputURL);
+        }
     }
 
     let inputText: string;
