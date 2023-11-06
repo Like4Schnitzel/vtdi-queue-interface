e8d40f2e523ff2de1195a7eeb56964cbcb74cd7d
Like4Schnitzel <rw21062006@gmail.com>
2023-11-06 19:59:03 +0100

    create framework

---
+++ README.md
@@ -0,0 +1,1 @@
+A web interface to submit URLs for my [video-to-terminal](https://github.com/Like4Schnitzel/video-to-terminal) project to play.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+
+</script>
+
+<body>
+
+</body>
+
+<style>
+    body {
+        height: 100%;
+        width: 100%;
+        background-attachment: fixed;
+        background-repeat: no-repeat;
+        background-image: linear-gradient(#00d9ff, #fe00ff);
+    }
+</style>
