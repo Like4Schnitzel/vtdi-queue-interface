09a6500eaf1de0bd30509495f0a1f3155f6e282b
Schnitzel <rw21062006@gmail.com>
2023-11-08 16:29:09 +0100

    fix horizontal scroll

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,6 @@
 <style>
     body {
         height: 100%;
-        width: 100%;
         background-attachment: fixed;
         background-repeat: no-repeat;
         background-image: linear-gradient(#00d9ff, #fe00ff);
