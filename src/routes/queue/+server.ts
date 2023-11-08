27797667d66f52d86b4ca577f90405d2e8e6694a
Schnitzel <rw21062006@gmail.com>
2023-11-08 22:30:21 +0100

    implement patch password checking

--- src/routes/queue/+server.ts
+++ src/routes/queue/+server.ts
@@ -1,6 +1,7 @@
 import { queue } from "$lib/queue";
 import type { RequestHandler } from "@sveltejs/kit";
 import { json } from "@sveltejs/kit";
+import { sha256 } from 'js-sha256';
 
 export const GET: RequestHandler = async () => {
     return json(queue);
@@ -9,5 +10,16 @@ export const GET: RequestHandler = async () => {
 export const POST: RequestHandler = async ({ request }) => {
     const body = await request.json();
     queue.push(body);
-    return json(queue);
+    return json({ status: 201 });
+};
+
+export const PATCH: RequestHandler = async ({ request }) => {
+    const body = await request.json();
+    const pw: string = body['password'];
+    // sha256 hash of correct password is 90d9e6c2489d8a428f403f2d01f406146946bf3699039a3763a2601226c47429
+    if (sha256(pw) === "90d9e6c2489d8a428f403f2d01f406146946bf3699039a3763a2601226c47429") {
+        return json({ status: 200 });
+    }
+
+    return json({ status: 403 });
 };
